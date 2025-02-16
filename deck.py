6870ec64ccb3c9575c32dfc0d28318887f22fcd8
Quinn Luetzow <quinn.luetzow@gmail.com>
2025-02-16 09:21:36 -0600

    Initial setup.

---
+++ deck.py
@@ -0,0 +1,18 @@
+# Blackjack Copyright (C) 2019-2024 Quinn Luetzow
+# This file is part of Standard Fantasy Character Generator.
+
+# Blackjack is free software: you can
+# redistribute it and/or modify it under the terms of the GNU General
+# Public License as published by the Free Software Foundation, either
+# version 3 of the License, or (at your option) any later version.
+
+# Blackjack is distributed in the hope that
+# it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+# the GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Blackjack. If not, see
+# <https://www.gnu.org/licenses/>.
+
+#!/usr/bin/env python3
