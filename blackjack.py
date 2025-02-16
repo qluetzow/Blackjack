da9e802ed7fd1695d9489013178834a221af5120
Quinn Luetzow <quinn.luetzow@gmail.com>
2025-02-16 15:50:52 -0600

    Implementation of Card and Deck classes.

--- blackjack.py
+++ blackjack.py
@@ -25,5 +25,42 @@ Usage: python blackjack.py
 __author__ = "Quinn Luetzow"
 __version__ = 0.1
 
+
+from random import shuffle
+
+
+class Card:
+    def __init__(self, suit, value):
+        self.self = self
+        self.suit = suit
+        self.value = value
+
+
+class Shoe:
+    suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
+    values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
+
+    def __init__(self, deck_count=1):
+        self.self = self
+        self.deck_count = deck_count
+        self.shoe = []
+        self.shuffle()
+
+    def shuffle(self):
+        self.shoe = []
+        for i in range(0, self.deck_count):
+            for j in Shoe.suits:
+                for k in Shoe.values:
+                    self.shoe.append(Card([j], [k]))
+        shuffle(self.shoe)
+
+    def pull_card(self):
+        try:
+            return self.shoe.pop()
+        except IndexError:
+            print("Out of cards - must reshuffle.")
+
+
+
 if __name__ == "__main__":
     exit(0)
